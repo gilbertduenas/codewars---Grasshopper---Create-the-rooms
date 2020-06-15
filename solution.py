33dcd5f4dfd149e1d1e776557f0e7513d572b260
Gilbert <gilbertduenas@users.noreply.github.com>
2020-06-15 22:15:29 +0900

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,5 @@
+rooms = {
+    'room1': {'name': 'gold', 'description': 'level1', 'completed': True},
+    'room2': {'name': 'platinum', 'description': 'level2', 'completed': True},
+    'room3': {'name': 'titanium', 'description': 'level3', 'completed': False}
+    }
